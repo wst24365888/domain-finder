c049c5d5c8e28dfab9b477e5e17b358e978f459e
Xyphuz <hansen0715.2@gmail.com>
2021-12-24 02:52:08 +0800

    feat: worker pull, single http client

--- domain_finder.go
+++ domain_finder.go
@@ -6,67 +6,22 @@ import (
 	"net/http"
 	"os"
 	"regexp"
-	"strconv"
+	"runtime"
 	"time"
 )
 
-type DomainStatus struct {
-	domain    string
+type Domain struct {
+	name      string
 	available bool
-	message   string
 }
 
-func worker(jobs chan string, results chan DomainStatus) {
-	for job := range jobs {
-		client := &http.Client{}
-
-		req, err := http.NewRequest("GET", "https://www.domainnamesoup.com/cell3.php?domain="+job+"&pt=86", nil)
-		req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36")
-		req.Header.Set("Referer", "https://www.google.com")
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		resp, err := client.Do(req)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		defer resp.Body.Close()
-
-		page, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Println(err)
-		}
-
-		result := (string(page) == "-1")
-		d := DomainStatus{domain: job, available: result, message: job + ": " + strconv.FormatBool(result)}
-		results <- d
-	}
-}
-
-func main() {
-	const WORKER_AMOUNT int = 10
+func dispatcher(jobChan chan Domain, closed chan bool) {
 	const TLD string = ".com"
 
 	// Go doesn't support const arrays (or slices)
 	ALPHABETS := []string{"-", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
-	JOB_AMOUNT := len(ALPHABETS) * len(ALPHABETS) * len(ALPHABETS) * len(ALPHABETS)
-	var ACTUAL_JOB_AMOUNT int = 0
 
-	now := time.Now()
-	f, err := os.Create("./results/domains-available-" + now.Format("2006-02-01T15-04-05") + ".txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	jobs := make(chan string, JOB_AMOUNT)
-	results := make(chan DomainStatus, JOB_AMOUNT)
-
-	for workerIndex := 0; workerIndex < WORKER_AMOUNT; workerIndex++ {
-		go worker(jobs, results)
-	}
+	defer close(jobChan)
 
 	for _, firstLetter := range ALPHABETS {
 		for _, secondLetter := range ALPHABETS {
@@ -76,23 +31,80 @@ func main() {
 					match, _ := regexp.MatchString("(([a-z0-9][a-z0-9][a-z0-9\\-][a-z0-9])|([a-z0-9][a-z0-9\\-][a-z0-9][a-z0-9]))\\.com", domain)
 
 					if match {
-						jobs <- domain
-						ACTUAL_JOB_AMOUNT++
+						jobChan <- Domain{domain, false}
 					}
 				}
 			}
 		}
 	}
 
-	close(jobs)
+	closed <- true
+}
 
-	for r := 1; r <= ACTUAL_JOB_AMOUNT; r++ {
-		d := <-results
+func workerPool(max int, jobChan chan Domain, respChan chan Domain) {
+	t := &http.Transport{}
+	for i := 0; i < max; i++ {
+		go worker(t, jobChan, respChan)
+	}
+}
 
-		if d.available {
-			f.WriteString(d.domain + "\n")
+func worker(t *http.Transport, jobChan chan Domain, respChan chan Domain) {
+	for d := range jobChan {
+		req, err := http.NewRequest("GET", "https://www.domainnamesoup.com/cell3.php?domain="+d.name+"&pt=86", nil)
+		if err != nil {
+			log.Println(err)
+			continue
 		}
+
+		req.Header.Set("Referer", "https://www.google.com")
+		req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36")
+
+		resp, err := t.RoundTrip(req)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		defer resp.Body.Close()
+
+		page, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Println(err)
+		}
+
+		d.available = (string(page) == "-1")
+		respChan <- d
+	}
+}
+
+func consumer(respChan chan Domain, closed chan bool) {
+	now := time.Now()
+	f, err := os.Create("./results/domains-available-" + now.Format("2006-02-01T15-04-05") + ".txt")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	close(results)
+	for {
+		select {
+		case <-closed:
+			break
+		case d := <-respChan:
+			if d.available {
+				f.WriteString(d.name + "\n")
+			}
+		}
+	}
+}
+
+func main() {
+	const WORKER_AMOUNT int = 25
+	const BUFFER_SIZE int = 100
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	jobChan := make(chan Domain, BUFFER_SIZE)
+	respChan := make(chan Domain, BUFFER_SIZE)
+	closed := make(chan bool)
+
+	go workerPool(WORKER_AMOUNT, jobChan, respChan)
+	go dispatcher(jobChan, closed)
+	consumer(respChan, closed)
 }
