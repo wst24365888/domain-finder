e7fcbaa48f7062d7a744c5f3c2353cbbaccaca6d
Xyphuz <hansen0715.2@gmail.com>
2021-12-24 01:42:52 +0800

    feat: up to 4-letter domain, keep result in a txt file

--- domain_finder.go
+++ domain_finder.go
@@ -1,18 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"regexp"
 	"strconv"
+	"time"
 )
 
-func worker(jobs chan string, results chan string) {
+type DomainStatus struct {
+	domain    string
+	available bool
+	message   string
+}
+
+func worker(jobs chan string, results chan DomainStatus) {
 	for job := range jobs {
 		client := &http.Client{}
 
 		req, err := http.NewRequest("GET", "https://www.domainnamesoup.com/cell3.php?domain="+job+"&pt=86", nil)
+		req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36")
+		req.Header.Set("Referer", "https://www.google.com")
 		if err != nil {
 			log.Println(err)
 			return
@@ -31,7 +41,8 @@ func worker(jobs chan string, results chan string) {
 		}
 
 		result := (string(page) == "-1")
-		results <- job + ": " + strconv.FormatBool(result)
+		d := DomainStatus{domain: job, available: result, message: job + ": " + strconv.FormatBool(result)}
+		results <- d
 	}
 }
 
@@ -40,11 +51,18 @@ func main() {
 	const TLD string = ".com"
 
 	// Go doesn't support const arrays (or slices)
-	ALPHABETS := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
-	JOB_AMOUNT := len(ALPHABETS) * 1 * len(ALPHABETS)
+	ALPHABETS := []string{"-", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	JOB_AMOUNT := len(ALPHABETS) * len(ALPHABETS) * len(ALPHABETS) * len(ALPHABETS)
+	var ACTUAL_JOB_AMOUNT int = 0
+
+	now := time.Now()
+	f, err := os.Create("./results/domains-available-" + now.Format("2006-02-01T15-04-05") + ".txt")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	jobs := make(chan string, JOB_AMOUNT)
-	results := make(chan string, JOB_AMOUNT)
+	results := make(chan DomainStatus, JOB_AMOUNT)
 
 	for workerIndex := 0; workerIndex < WORKER_AMOUNT; workerIndex++ {
 		go worker(jobs, results)
@@ -52,14 +70,28 @@ func main() {
 
 	for _, firstLetter := range ALPHABETS {
 		for _, secondLetter := range ALPHABETS {
-			jobs <- firstLetter + "-" + secondLetter + TLD
+			for _, thirdLetter := range ALPHABETS {
+				for _, fourthLetter := range ALPHABETS {
+					domain := firstLetter + secondLetter + thirdLetter + fourthLetter + TLD
+					match, _ := regexp.MatchString("(([a-z0-9][a-z0-9][a-z0-9\\-][a-z0-9])|([a-z0-9][a-z0-9\\-][a-z0-9][a-z0-9]))\\.com", domain)
+
+					if match {
+						jobs <- domain
+						ACTUAL_JOB_AMOUNT++
+					}
+				}
+			}
 		}
 	}
 
 	close(jobs)
 
-	for r := 1; r <= JOB_AMOUNT; r++ {
-		fmt.Println(<-results)
+	for r := 1; r <= ACTUAL_JOB_AMOUNT; r++ {
+		d := <-results
+
+		if d.available {
+			f.WriteString(d.domain + "\n")
+		}
 	}
 
 	close(results)
